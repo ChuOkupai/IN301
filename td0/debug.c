6c0ba7279e73cc174ca351b4b4041b10eab46734
Adrien <adrienadrien@neuf.fr>
2018-09-17 11:51:40 +0200

    ajout ex05

--- td0/debug.c
+++ td0/debug.c
@@ -2,53 +2,50 @@
 #include <stdlib.h>
 #include <time.h>
 
-
 //fonction factorielle
-int factorielle(int x){
-  int res;
-  if(x == 0) return 0;
-  while(x > 1){
-    res *= (x-1);
-    x = x-1;
-  }
-  return res;
+int factorielle(int x)
+{
+	if (x == 0)
+		return 1;
+	else return x * factorielle(x - 1);
 }
 
 //fonction somme
-int somme(int x){
-  int res = 0;
-  int i = 0;
-  while(i <= x){
-    i++;       /*incrementation 
-    res += i-1;/* fin de boucle */
-  }
-  return res;
+int somme(int x)
+{
+	int res = 0;
+	int i = 0;
+	while (i <= x)
+	{
+    	i++; /*incrementation */ 
+    	res += i - 1; /* fin de boucle */
+	}
+	return res;
 }
 
 //fonction maximum
-int maximum(int x, int y){
-  if(x = y || y > x)
-    return y;
-  else 
-    return x;
+int maximum(int x, int y)
+{
+	return (x > y) ? x : y;
 }
 
 //fonction principale
-int main(){
-  int a, b;
+int main()
+{
+	int a, b;
 
-  srand(time(NULL));
-  a = rand() % 8 + 2;
-  b = factorielle(a);
-  printf("\nFactorielle de %d vaut %d\n\n", a, b);
-  
-  a = rand() % 100;
-  b = somme(a);
-  printf("\nLa somme des entiers de 1 a %d vaut %d\n\n", a, b);
+	srand(time(NULL));
+	a = rand() % 8 + 2;
+	b = factorielle(a);
+	printf("\nFactorielle de %d vaut %d\n\n", a, b);
 
-  a = rand() % 100;
-  b = rand() % 100;
-  printf("\nLe maximum entre %d et %d vaut %d\n\n", a, b, maximum(a,b));
-  
-  return 0;
+	a = rand() % 100;
+	b = somme(a);
+	printf("\nLa somme des entiers de 1 a %d vaut %d\n\n", a, b);
+	
+	a = rand() % 100;
+	b = rand() % 100;
+	printf("\nLe maximum entre %d et %d vaut %d\n\n", a, b, maximum(a,b));
+
+	return 0;
 }
